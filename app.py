a09daec1d0cfae26b9b2b8ffdcf2cbb987cbff1e
sudomando1130 <53624749+sudomando1130@users.noreply.github.com>
2026-06-30 13:17:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st 
 import pandas as pd 
-#from ydata_profiling import ProfileReport
+from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
-import data_profiling
+#import data_profiling
 from data_profiling import ProfileReport
 import sys
 import os
