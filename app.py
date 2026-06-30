a55eb5e88039af11461a6a28fc8e8551832f5788
sudomando1130 <53624749+sudomando1130@users.noreply.github.com>
2026-06-30 12:13:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,58 +19,130 @@ def validate_file(file):
         return ext
     else:
         return False
-    
 
-# sidebar
+
+
+
+#Side Bar
 with st.sidebar:
-    uploaded_file = st.file_uploader("Upload .csv, .xlsx files not exceeding 10 MB")
+    uploaded_file = st.file_uploader('upload .csv or .xlsx file not exceeding 10MB')
     if uploaded_file is not None:
-        st.write('Modes of Operation')
-        minimal = st.checkbox('Do you want minimal report ?')
-        display_mode = st.radio('Display mode:',
-                                options=('Primary','Dark','Orange'))
+        st.write('Modes Of Operation')
+        minimal = st.checkbox('Do you want Minimal Report?')
+        display_mode = st.radio('Display mode:', options = ('Primary', 'Dark', 'Orange'))
+        
         if display_mode == 'Dark':
-            dark_mode= True
-            orange_mode = False
+            dark_mode = True
+            orange_mode = False 
         elif display_mode == 'Orange':
-            dark_mode = False
+            dark_mode = False 
             orange_mode = True
         else:
             dark_mode = False
-            orange_mode = False
-        
+            orange_mode = False 
     
+
 if uploaded_file is not None:
     ext = validate_file(uploaded_file)
     if ext:
         filesize = get_filesize(uploaded_file)
-        if filesize <= 10:
+        if filesize <= 10:            
             if ext == '.csv':
-                # time being let load csv
-                df = pd.read_csv(uploaded_file)
+                # time being let just load .CSV
+                df = pd.read_csv(uploaded_file,encoding='utf-8')
             else:
                 xl_file = pd.ExcelFile(uploaded_file)
                 sheet_tuple = tuple(xl_file.sheet_names)
-                sheet_name = st.sidebar.selectbox('Select the sheet',sheet_tuple)
+                sheet_name = st.sidebar.selectbox('Select the sheet', sheet_tuple)
                 df = xl_file.parse(sheet_name)
-                
-                
             # generate report
             with st.spinner('Generating Report'):
-                pr = ProfileReport(df,
-                                minimal=minimal,
-                                dark_mode=dark_mode,
-                                orange_mode=orange_mode
-                                )
-                
-            st_profile_report(pr)
-        else:
-            st.error(f'Maximum allowed filesize is 10 MB. But received {filesize} MB')
+                pr = ProfileReport(df,minimal=minimal)
             
+            st_profile_report(pr)
+            
+        else:
+            st.error('Maxium Allowed filesize is 10 MB. but recieved {filesize} MB')
     else:
-        st.error('Kindly upload only .csv or .xlsx file')
-        
+        st.error('Kindly upload only .CSV or .XLSX file ')
+
 else:
     st.title('Data Profiler')
-    st.info('Upload your data in the left sidebar to generate profiling')
+    st.info('Upload your data in the life sidebar to generate profiling')
+    
+
+
+
+
+
+
+
+
+
+# def get_filesize(file):
+#     size_bytes = sys.getsizeof(file)
+#     size_mb = size_bytes / (1024**2)
+#     return size_mb
+
+# def validate_file(file):
+#     filename = file.name
+#     name, ext = os.path.splitext(filename)
+#     if ext in ('.csv','.xlsx'):
+#         return ext
+#     else:
+#         return False
+    
+
+## sidebar
+# with st.sidebar:
+#     uploaded_file = st.file_uploader("Upload .csv, .xlsx files not exceeding 10 MB")
+#     if uploaded_file is not None:
+#         st.write('Modes of Operation')
+#         minimal = st.checkbox('Do you want minimal report ?')
+#         display_mode = st.radio('Display mode:',
+#                                 options=('Primary','Dark','Orange'))
+#         if display_mode == 'Dark':
+#             dark_mode= True
+#             orange_mode = False
+#         elif display_mode == 'Orange':
+#             dark_mode = False
+#             orange_mode = True
+#         else:
+#             dark_mode = False
+#             orange_mode = False
+        
+    
+# if uploaded_file is not None:
+#     ext = validate_file(uploaded_file)
+#     if ext:
+#         filesize = get_filesize(uploaded_file)
+#         if filesize <= 10:
+#             if ext == '.csv':
+#                 # time being let load csv
+#                 df = pd.read_csv(uploaded_file)
+#             else:
+#                 xl_file = pd.ExcelFile(uploaded_file)
+#                 sheet_tuple = tuple(xl_file.sheet_names)
+#                 sheet_name = st.sidebar.selectbox('Select the sheet',sheet_tuple)
+#                 df = xl_file.parse(sheet_name)
+                
+                
+#             # generate report
+#             with st.spinner('Generating Report'):
+#                 pr = ProfileReport(df,
+#                                 minimal=minimal,
+#                                 dark_mode=dark_mode,
+#                                 orange_mode=orange_mode
+#                                 )
+                
+#             st_profile_report(pr)
+#         else:
+#             st.error(f'Maximum allowed filesize is 10 MB. But received {filesize} MB')
+            
+#     else:
+#         st.error('Kindly upload only .csv or .xlsx file')
+        
+# else:
+#     st.title('Data Profiler')
+#     st.info('Upload your data in the left sidebar to generate profiling')
     
